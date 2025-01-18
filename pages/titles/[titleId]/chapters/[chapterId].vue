0f533d741561e96eda4b3dd329d3f180266ac9f1
Timolio <94037690+Timolio@users.noreply.github.com>
2025-01-18 01:34:49 +0100

    Attempt 2

--- pages/titles/[titleId]/chapters/[chapterId].vue
+++ pages/titles/[titleId]/chapters/[chapterId].vue
@@ -220,6 +220,26 @@ onMounted(async () => {
     //     });
     //     AdController.show();
     // }
+    document.body.addEventListener(
+        'focus',
+        event => {
+            const target = event.target;
+            switch (target.tagName) {
+                case 'INPUT':
+                case 'TEXTAREA':
+                case 'SELECT':
+                    document.body.classList.add('keyboard');
+            }
+        },
+        true
+    );
+    document.body.addEventListener(
+        'blur',
+        () => {
+            document.body.classList.remove('keyboard');
+        },
+        true
+    );
     await chapterStore.fetchTitle(titleId, initDataUnsafe?.user?.id || 404);
     await chapterStore.fetchFullChapter(chapterId);
     currentPage.value = 1;
